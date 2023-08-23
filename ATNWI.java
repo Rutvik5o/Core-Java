f66965f64572a54a243a77f4f7897ff9d093b00d
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-08-23 14:53:57 +0530

    Add Two Number Without User Input

---
+++ ATNWI.java
@@ -0,0 +1,9 @@
+// Add Two Number Without user input
+class ATNWI
+{
+     public static void main(String args[])
+     {
+         int a=20,b=40;
+         System.out.println("Sum= "+(a+b));
+     }
+}
