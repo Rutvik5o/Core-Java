2b500355e30a33bc94f03c58620a4aa5be9fc9c0
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-20 20:40:28 +0530

    University->1

---
+++ U1.java
@@ -0,0 +1,14 @@
+
+class U1
+{
+    public static void main(String []args)
+    {
+        float p,r,t,si;
+
+        p=13000; r=12; t=2;
+
+        si=(p*r*t)/100;
+
+        System.out.println("Simple Interest=> "+si);
+    }
+}
