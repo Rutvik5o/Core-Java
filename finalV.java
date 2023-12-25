7b3ec1dac0eea1bc087646940068c297873544b2
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-25 23:15:34 +0530

    Final Variable

---
+++ finalV.java
@@ -0,0 +1,13 @@
+class finalV
+{
+    public static void main(String []args)
+    {
+        final int A=10;
+
+        System.out.println(A);
+
+    //    A=10; can't modify due to final keyword
+
+        System.out.println(A);
+    }
+}
