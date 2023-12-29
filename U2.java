76d385ff11934f41c7cc655254a8cea1c1dff182
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-29 18:31:05 +0530

    University->2

--- U2.java
+++ U2.java
@@ -1,4 +1,6 @@
-
+//(2)=> A motor cycle dealer sells to two-wheelers to his customer on loan,which is to be repaid in 5 years. The dealer charges simple interest for the whole term on the 
+//day of giving the loan itself. The total amount is then divided by 60(months) and is collected as equated monthly installment(EMI).Write a program to calculate
+//the EMI for a loan of Rs. X, where X is given from command line argument. Print the EMI value in rupees.
 class U2
 {
     public static void main(String []args)
