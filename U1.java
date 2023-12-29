5fd32795e76ed626d4e570d0e23f25cb48ee1e0b
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2023-12-29 18:40:12 +0530

    Update U1.java

--- U1.java
+++ U1.java
@@ -1,14 +1,1 @@
 //(1)=> write a program to evulate simple interest of a given principle, rate and time.
-class U1
-{
-    public static void main(String []args)
-    {
-        float p,r,t,si;
-
-        p=13000; r=12; t=2;
-
-        si=(p*r*t)/100;
-
-        System.out.println("Simple Interest=> "+si);
-    }
-}
