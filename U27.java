8795cd3075c022dd30355149a76830e1660a206f
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-01-06 10:45:47 +0530

    University:Ch:2-(7)

---
+++ U27.java
@@ -0,0 +1,37 @@
+//(7>)=>Delcare an abstract class Vehicle wiwth an abstract methods name numWheels(). Provide subclasses Car and Truck that
+//each implements this methods. Create instance of these subclasses and demostrate the use of this method.
+
+abstract class Vehicle
+{
+    abstract void numWheels();
+}
+
+class car extends Vehicle
+{
+    void numWheels()
+    {
+        System.out.println("Car Class with four wheels");
+    }
+}
+
+class truck extends Vehicle
+{
+    void numWheels()
+    {
+        System.out.println("Truck class with six wheels");
+    }
+}
+
+class U27
+{
+    public static void main(String []args)
+    {
+        car Obj1=new car();
+        Obj1.numWheels();
+
+        truck Obj2=new truck();
+        Obj2.numWheels();
+
+
+    }
+}
