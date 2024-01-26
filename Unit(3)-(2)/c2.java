4bb5fa9dc8e6ea91d2ec4af90f3f83e9aecba6b0
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-01-26 15:36:51 +0530

    Delete Unit(3)-(2) directory

--- Unit(3)-(2)/c2.java
+++
@@ -1,9 +0,0 @@
-package p2;
-
-public class c2
-{
-    public void display()
-    {
-        System.out.println("Class c2 of the Package c2");
-    }
-}
--- Unit(3)-(2)/c3.java
+++
@@ -1,9 +0,0 @@
-package p3;
-
-public class c3
-{
-    public void display()
-    {
-         System.out.println("Class c3 of the Packagee p3");
-    }
-}
