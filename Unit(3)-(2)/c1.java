31163f247c9122f522388aa38ea17fbc410cda81
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-01-26 15:38:01 +0530

    University;Ch:3-(2)

---
+++ Unit(3)-(2)/c1.java
@@ -0,0 +1,11 @@
+//(2)=> Write a program that creates three different classes in three different packages
+//and access them from default package. All the hree packages should be at the same level.
+package p1;
+
+public class c1
+{
+    public void display()
+    {
+        System.out.println("Class c1 of the Package p1");
+    }
+}
